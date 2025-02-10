09eca97d8c93f32b986605df2ac7db731811defb
bhandekunal16 <bhandekunal16@gmail.com>
2025-02-10 10:31:01 +0530

    `Refactor todo list component: update HTML and TypeScript code to add new features`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideHeaderComponent } from "./side-header/side-header.component";
+import { SideHeaderComponent } from './side-header/side-header.component';
 import { BookmarkComponent } from './bookmark/bookmark.component';
 import { SearchComponent } from './search/search.component';
 import { Refund } from '../models/refund.model';
@@ -10,40 +10,52 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [ CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todo-app';
 
-  refund:Refund={
-    refundAmount:1500,
-    refundDate:'01-01-2025',
-    refundStatus:false
+  public fullName: string = 'chandan';
+  public description: string = '';
+  public date: string = '';
+  public isCompleted: boolean = false;
+  
+  public refund: Refund = {
+    refundAmount: 1500,
+    refundDate: '01-01-2025',
+    refundStatus: false,
   };
+  public todos: ToDO[] = [
+    {
+      description: 'Buy Groceries',
+      date: '01-02-2025',
+      isCompleted: false,
+    },
+    {
+      date: '01-02-2025',
+      description: 'Buy Pencil',
+      isCompleted: false,
+    },
+    {
+      description: 'Sell Old Newspaper',
+      date: '01-02-2025',
+      isCompleted: false,
+    },
+  ];
 
-  todos:ToDO[]=[{
-    description:'Buy Groceries',
-    date:'01-02-2025',
-    isCompleted:false
-  }, {
-    date:'01-02-2025',
-    description:'Buy Pencil',
-    isCompleted:false,
-  }, {
-    description:'Sell Old Newspaper',
-    date:'01-02-2025',
-    isCompleted:false
-  }]
+  constructor() {}
 
-  fullName:string="chandan";
-
-  constructor(){
+  public addIntoArray() {
+    this.todos.push({
+      date: this.date,
+      description: this.description,
+      isCompleted: this.isCompleted,
+    });
   }
 
-
-  showFullName(){
+  public showFullName() {
     alert(this.fullName);
   }
 }
